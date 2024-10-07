2a2b32d05d5857d08d03f744b24797fb777ae7b4
JR Bussard <scottbussardjr@gmail.com>
2024-10-07 06:22:43 -0400

    added displayBooks function
    added a function to take all the books in the array and put them in a formatted card that will display well.

--- index.js
+++ index.js
@@ -14,3 +14,27 @@ function Book(title, author, pages, read) {
 function addBook(title, author, pages, read) {
     library.push(new Book(title, author, pages, read));
 }
+
+function displayBooks(layout, library) {
+    for (let i in library) {
+        const card = templateCard.cloneNode(true);
+        card.classList.remove("hidden-card");
+        card.querySelector("#title").textContent = library[i].title;
+        card.querySelector("#author").textContent = library[i].author;
+        card.querySelector("#pages").textContent = library[i].pages + " Pages";
+        if (library[i].read) {
+            card.querySelector("#read-status").textContent = "Book is Read";
+        } else {
+            card.querySelector("#read-status").textContent = "Book is not Read";
+        }
+        layout.appendChild(card);
+    }
+}
+
+const cardLayout = document.querySelector(".card-layout");
+
+const templateCard = document.querySelector("#template");
+
+addBook("Atomic Habits", "James Clear", 100, false);
+addBook("Winning the War in Your Mind", "Craig Groeshel", 200, true);
+displayBooks(cardLayout, library);
