8b647a9335bf37337ace6ff4da54866af78dd47d
JR Bussard <scottbussardjr@gmail.com>
2024-10-12 13:37:26 -0400

    Added the toggle and delete logic
    Added event listeners to toggle and delete, also refactored to make the book into a class and not just an object

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const library = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.index;
+    }
+    toggleRead() {
+        this.read = !this.read;
+    }
 
-    function toggleRead() {
-        read = !read;
+    displayBook(num) {
+        this.index = num;
+        const card = document.querySelector("#template").cloneNode(true);
+        card.classList.remove("hidden-card");
+        card.querySelector("#title").textContent = this.title;
+        card.querySelector("#author").textContent = this.author;
+        card.querySelector("#pages").textContent = this.pages + " Pages";
+        card.querySelector("#toggle").value = this.index;
+        card.querySelector("#delete").value = this.index;
+        if (this.read) {
+            card.querySelector("#read-status").textContent = "Book is Read";
+        } else {
+            card.querySelector("#read-status").textContent = "Book is not Read";
+        }
+        return card;
     }
 }
 
@@ -15,20 +34,33 @@ function addBook(title, author, pages, read) {
     library.push(new Book(title, author, pages, read));
 }
 
-function displayBooks(layout, library) {
-    for (let i in library) {
-        const card = templateCard.cloneNode(true);
-        card.classList.remove("hidden-card");
-        card.querySelector("#title").textContent = library[i].title;
-        card.querySelector("#author").textContent = library[i].author;
-        card.querySelector("#pages").textContent = library[i].pages + " Pages";
-        if (library[i].read) {
-            card.querySelector("#read-status").textContent = "Book is Read";
-        } else {
-            card.querySelector("#read-status").textContent = "Book is not Read";
-        }
-        layout.appendChild(card);
+function displayBooks(layout) {
+    while (layout.firstChild) {
+        layout.removeChild(layout.lastChild);
     }
+    for (let i in library) {
+        layout.appendChild(library[i].displayBook(i));
+    }
+    document.querySelectorAll("#toggle").forEach((element) => {
+        element.addEventListener("click", (event) => {
+            toggleReadBtn(element.value);
+        });
+    });
+    document.querySelectorAll("#delete").forEach((element) => {
+        element.addEventListener("click", (event) => {
+            deleteCard(element.value);
+        });
+    });
+}
+
+function toggleReadBtn(index) {
+    library[index].toggleRead();
+    displayBooks(cardLayout);
+}
+
+function deleteCard(index) {
+    library.splice(index, 1);
+    displayBooks(cardLayout);
 }
 
 const addBookForm = document.querySelector("#dialog-form");
@@ -36,6 +68,7 @@ const addBookDialog = document.querySelector("#book-dialog");
 const addBookBtn = document.querySelector("#new-book");
 const cardLayout = document.querySelector(".card-layout");
 const templateCard = document.querySelector("#template");
+const deleteBookButton = document.querySelectorAll("#delete");
 
 addBookBtn.addEventListener("click", (event) => {
     addBookDialog.showModal();
@@ -55,10 +88,11 @@ addBookForm.addEventListener("submit", (event) => {
 
     addBookForm.reset();
     addBookDialog.close();
+    displayBooks(cardLayout, library);
 
     event.preventDefault();
 });
 
 addBook("Atomic Habits", "James Clear", 100, false);
 addBook("Winning the War in Your Mind", "Craig Groeshel", 200, true);
-displayBooks(cardLayout, library);
+displayBooks(cardLayout);
